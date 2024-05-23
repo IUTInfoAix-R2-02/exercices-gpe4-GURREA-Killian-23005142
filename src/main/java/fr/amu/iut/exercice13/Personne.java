180bf040eba0896492f4ad26c52b869a4c2f9c61
Killian.G <145963835+GURREA-Killian-23005142@users.noreply.github.com>
2024-05-23 12:08:34 +0200

    exo13 et 14

--- src/main/java/fr/amu/iut/exercice13/Personne.java
+++ src/main/java/fr/amu/iut/exercice13/Personne.java
@@ -1,17 +1,19 @@
-package fr.amu.iut.exercice3;
+package fr.amu.iut.exercice13;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Personne {
 
     private String nom;
-    private int age;
+    private IntegerProperty age;
     private StringProperty villeDeNaissance;
 
     public Personne(String nom, int age) {
         this.nom = nom;
-        this.age = age;
+        this.age = new SimpleIntegerProperty(age);
         this.villeDeNaissance = new SimpleStringProperty("Paris");
     }
 
@@ -19,12 +21,27 @@ public class Personne {
         return nom;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public int getAge() {
+        return age.get();
     }
 
-    public int getAge() {
+    public IntegerProperty ageProperty() {
         return age;
     }
 
+    public void setAge(int age) {
+        this.age.set(age);
+    }
+
+    public String getVilleDeNaissance() {
+        return villeDeNaissance.get();
+    }
+
+    public StringProperty villeDeNaissanceProperty() {
+        return villeDeNaissance;
+    }
+
+    public void setVilleDeNaissance(String ville) {
+        this.villeDeNaissance.set(ville);
+    }
 }
