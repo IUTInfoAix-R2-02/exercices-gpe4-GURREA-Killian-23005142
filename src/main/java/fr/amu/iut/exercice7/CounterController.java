1e267807781aa6f8eee301b032c05302f51039c9
Killian.G <145963835+GURREA-Killian-23005142@users.noreply.github.com>
2024-05-29 08:42:17 +0200

    exo 7 (probleme de push)

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,22 +1,36 @@
 package fr.amu.iut.exercice7;
 
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
-    int counter = 0;
+    private int counter = 0;
+
+    @FXML
+    Label counterLabel;
+
+    @FXML
+    Button incrementButton;
+
+    @FXML
+    Button decrementButton;
 
     public void increment() {
+        counterLabel.setText(String.valueOf(counter++));
     }
 
     public void decrement() {
+        counterLabel.setText(String.valueOf(counter--));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
-   }
+    }
 }
